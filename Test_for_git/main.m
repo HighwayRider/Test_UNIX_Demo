082880a69a0fb118428231876230563b9ee2477a
HighwayRider <stuntpiloff2b@mail.com>
2014-08-01 17:01:07 +0300

    Initial Commit

---
+++ Test_for_git/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Test_for_git
+//
+//  Created by Dmytro Pavlovskyi on 01.08.14.
+//  Copyright (c) 2014 Dmytro Pavlovskyi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
