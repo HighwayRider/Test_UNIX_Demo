da876687688757474f2d2ce0e8c163b990a72e81
HighwayRider <stuntpiloff2b@mail.com>
2014-08-01 17:09:59 +0300

    Ver-2

--- Test_for_git/main.m
+++ Test_for_git/main.m
@@ -13,7 +13,7 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        NSLog(@"Hello, GIT!");
+        NSLog(@"Hello, GIT! Myhahahaha!!!!");
         
     }
     return 0;
